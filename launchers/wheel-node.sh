89c977f5b331e6c56e50f72e44f608ef2d1ec39b
aiden00713 <44128720+aiden00713@users.noreply.github.com>
2024-05-24 02:29:51 +0800

    Add files via upload

---
+++ launchers/wheel-node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# launch subscriber
+#rosrun my_package camera_node.py
+#sleep 5
+rosrun my_package wheel_node3.py
+# wait for app to end
+dt-launchfile-join
