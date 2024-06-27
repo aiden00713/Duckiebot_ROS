46a21bd3130dbe785453fceaf15422bf650d2837
aiden00713 <44128720+aiden00713@users.noreply.github.com>
2024-06-28 03:13:26 +0800

    Add files via upload

--- packages/my_package/src/wheel_node3.py
+++ packages/my_package/src/wheel_node3.py
@@ -2,20 +2,32 @@
 import os
 import math
 import rospy
-import cv2
-import numpy as np
-from pylsd2 import LineSegmentDetectionED
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import WheelsCmdStamped
-from sensor_msgs.msg import Range, CompressedImage
-from cv_bridge import CvBridge
+from sensor_msgs.msg import Range
 from std_msgs.msg import Float32, String
 
 # Angular velocities for each wheel (quarter rotation a second)
-W_LEFT = 1.2 * (2 * math.pi)  # 表示每秒左轮转 1.2 圈
-W_RIGHT = 1.2 * (2 * math.pi)  # 表示每秒右轮转 1.2 圈
+W_LEFT = 1.2 * (2 * math.pi)  # 表示每秒左輪轉 1.2 圈
+W_RIGHT = 1.2 * (2 * math.pi)  # 表示每秒右輪轉 1.2 圈
 TURN_SPEED = 0.5  # Adjust this value to control turn sharpness
-TURN_DURATION = 50  # Number of iterations to keep turning left
+
+class PIDController:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.integral = 0
+        self.previous_error = 0
+
+    def compute(self, setpoint, measured_value, dt):
+        error = setpoint - measured_value
+        self.integral += error * dt
+        derivative = (error - self.previous_error) / dt
+        self.previous_error = error
+        return self.kp * error + self.ki * self.integral + self.kd * derivative
+
+
 
 class WheelControlNode(DTROS):
     def __init__(self, node_name):
@@ -30,130 +42,101 @@ class WheelControlNode(DTROS):
         # Compute linear speeds
         self._vel_left = W_LEFT * wheel_radius
         self._vel_right = W_RIGHT * wheel_radius
-        self.counter = 0  # Initialize counter
 
         # Topics for ToF sensor and camera node angles & dist
         self._tof = f"/{self._vehicle_name}/front_center_tof_driver_node/range"
-        self._angle_topic = f"/{self._vehicle_name}/camera_node/angles"
-        self._right_dist_topic = f"/{self._vehicle_name}/camera_node/right_dist"
-        self._left_dist_topic = f"/{self._vehicle_name}/camera_node/left_dist"
+        self._straight_status_topic = f"/{self._vehicle_name}/camera_node/straight_status"
 
         # Construct publisher and subscriber
-        self.mul = 0
-        self._dis = 0
-        self._d_pre = 0
-        self.receiver = rospy.Subscriber("Duckie", String, self.call)
         self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
         self.distance = rospy.Subscriber(self._tof, Range, self.dis)
-        #self.angle_subscriber = rospy.Subscriber(self._angle_topic, Float32, self.angle_callback)
-        self.right_dist_subscriber = rospy.Subscriber(self._right_dist_topic, Float32, self.right_dist_callback)
-        self.left_dist_subscriber = rospy.Subscriber(self._left_dist_topic, Float32, self.left_dist_callback)
+        self.straight_status_subscriber = rospy.Subscriber(self._straight_status_topic, String, self.straight_status_callback)
+        self.angle_subscriber = rospy.Subscriber(f"/{self._vehicle_name}/camera_node/angle_pub", Float32, self.angle_callback)
 
-        # Register shutdown hook
-        rospy.on_shutdown(self.on_shutdown)
-        # 是否正在轉彎
-        self.is_turning = False
-    
-    def on_shutdown(self):
-        # This function is called when you press Ctrl+C
-        rospy.loginfo("Shutting down: Stopping the motors")
-        message = WheelsCmdStamped(vel_left=0, vel_right=0)
-        self._publisher.publish(message)
-        print("Shut down completed")
+        # State variable
+        self.state = "STRAIGHT"
+        self.turn_direction = "NONE"
+        self._dis = 0
 
-    def call(self, data):
-        self.mul = int(data.data)
+        # Initialize PID controller
+        self.pid = PIDController(kp=1.0, ki=0.1, kd=0.05)
+        self.last_time = rospy.get_time()
 
     def dis(self, data):
         self._d_pre = self._dis
         self._dis = int(1000 * data.range)
 
-    def adjust(self, par):
-        l = 0  # 左轮初始值
-        r = 0  # 右轮初始值
-
-        if par >= 100:
-            par = par * 0.32 - 23.6
-
-        if par <= -100:
-            par = (par / 10) - 2
-        if par >= 9:
-            l = par / 40 * self._vel_left
-        if par <= -9:
-            r = -par / 40 * self._vel_right
-        return l, r
-
     def turn_left(self):
-        if not self.is_turning:
-            left = self._vel_left * TURN_SPEED
-            right = self._vel_right
-            #message = WheelsCmdStamped(vel_left=left, vel_right=right)
-            message = WheelsCmdStamped(vel_left=0, vel_right=0.5)
-            self._publisher.publish(message)
-            self.is_turning = True
+        left = self._vel_left * TURN_SPEED
+        right = self._vel_right
+        message = WheelsCmdStamped(vel_left=left, vel_right=right)
+        self._publisher.publish(message)
 
     def turn_right(self):
-        if not self.is_turning:
-            rospy.loginfo("Turning left")
-            left = self._vel_left 
-            right = self._vel_right * TURN_SPEED
-            #essage = WheelsCmdStamped(vel_left=left, vel_right=right)
-            message = WheelsCmdStamped(vel_left=0.5, vel_right=0)
-            self._publisher.publish(message)
-            self.is_turning = True
+        left = self._vel_left 
+        right = self._vel_right * TURN_SPEED
+        message = WheelsCmdStamped(vel_left=left, vel_right=right)
+        self._publisher.publish(message)
 
     def forward(self):
         left = self._vel_left 
         right = self._vel_right
         message = WheelsCmdStamped(vel_left=left, vel_right=right)
         self._publisher.publish(message)
-        self.is_turning = False
+
+    def straight_status_callback(self, msg):
+        status, direction = msg.data.split(",")
+        self.state = status
+        self.turn_direction = direction
+        rospy.loginfo(f"Straight Status: {self.state}, Turn Direction: {self.turn_direction}")
+
+        if self.state == "TURN":
+            if self.turn_direction == "LEFT":
+                self.turn_left()
+            elif self.turn_direction == "RIGHT":
+                self.turn_right()
+        elif self.state == "STRAIGHT":
+            self.forward()
 
     def angle_callback(self, msg):
         angle = msg.data
-        rospy.loginfo(f"Received angle: {angle}")
-        if abs(angle) > 70:  # Assuming 70 degrees indicates a sharp turn
-            for _ in range(TURN_DURATION):
-                self.turn_left()
-    
-    # under 340 turn right
-    def right_dist_callback(self, msg):
-        dist = msg.data
-        rospy.loginfo(f"Received RIGHT inter. dist: {dist}")
-        if dist < 340:  
-            for _ in range(TURN_DURATION):
-                self.turn_right()
-        else:
-            for _ in range(TURN_DURATION):
-                self.forward()
-    
-    
-    def left_dist_callback(self, msg):
-        dist = msg.data
-        rospy.loginfo(f"Received LEFT inter. dist: {dist}")
-        if dist < 340:  
-            for _ in range(TURN_DURATION):
-                self.turn_left()
-        else:
-            for _ in range(TURN_DURATION):
-                self.forward()
+        current_time = rospy.get_time()
+        dt = current_time - self.last_time
+        self.last_time = current_time
 
-    
+        # Use PID controller to compute adjustment
+        adjustment = self.pid.compute(0, angle, dt)
+        self.adjust_wheels_based_on_angle(adjustment)
+
+    def adjust_wheels_based_on_angle(self, adjustment):
+        # Adjust the wheel speeds based on the PID adjustment
+        if adjustment > 0:  # Turn right
+            left = self._vel_left
+            right = self._vel_right * (1 - abs(adjustment) / 90)
+        else:  # Turn left
+            left = self._vel_left * (1 - abs(adjustment) / 90)
+            right = self._vel_right
+        message = WheelsCmdStamped(vel_left=left, vel_right=right)
+        self._publisher.publish(message)
+
+
     def run(self):
-        rate = rospy.Rate(10)
-        pre = 0
-        while not rospy.is_shutdown():
-            est_mul = pre * 0.1 + self.mul * 0.9
-            left, right = self.adjust(est_mul)
-            left = self._vel_left + left
-            right = self._vel_right + right
+        rospy.spin()
 
-            message = WheelsCmdStamped(vel_left=left, vel_right=right)
-            self._publisher.publish(message)
-            pre = self.mul
-            rate.sleep()
+    def on_shutdown(self):
+        if hasattr(self, '_publisher'):
+            stop = WheelsCmdStamped(vel_left=0, vel_right=0)
+            self._publisher.publish(stop)
+        print("Shut down completed")
 
 if __name__ == '__main__':
     node = WheelControlNode(node_name='wheel_control_node')
+    rospy.on_shutdown(node.on_shutdown)
     node.run()
-    rospy.spin()
+
+'''
+20240628
+1.新增pid控制
+2.得到camera_node的角度做修正
+
+'''
